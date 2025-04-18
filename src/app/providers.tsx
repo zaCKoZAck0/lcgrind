eb83df8113f92ae4488bb14caecca10a7736b1a9
Ayush Kumar Yadav <69889382+zaCKoZAck0@users.noreply.github.com>
2025-04-18 12:20:11 +0530

    Revert "theme toggle"

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,15 +2,12 @@
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '~/store';
-import { ThemeProvider } from 'next-themes';
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-                    {children}
-                </ThemeProvider>
+                {children}
             </PersistGate>
         </Provider>
     );
