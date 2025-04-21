d8b9d21cb2628c65af1837a305cc0da511666fd4
zackozack <id.ayushkryadav@gmail.com>
2025-04-21 04:19:20 +0000

    refactor: improve caching

--- src/app/all-problems/page.tsx
+++ src/app/all-problems/page.tsx
@@ -1,19 +1,7 @@
 import { ProblemsPage } from "~/components/all-problems/problems-page";
-import { SearchParams } from "~/types/problem";
 
-const ITEMS_PER_PAGE = 100;
+export const dynamic = "force-static";
 
-export default async function AllProblemsPage({
-    searchParams
-}: {
-    searchParams: Promise<SearchParams>;
-}) {
-    const params = await searchParams;
-    let { companies = null, tags = null } = params;
-    const { sort = 'question-id', order = 'all-problems', search = '', page = 1 } = params;
-    if (!Array.isArray(companies) && companies != null) companies = [companies];
-    if (!Array.isArray(tags) && tags != null) tags = [tags];
-
-
-    return (<ProblemsPage order={order} sort={sort} search={search} tags={tags} companies={companies} page={Number(page)} perPage={ITEMS_PER_PAGE} />)
+export default async function AllProblemsPage() {
+    return <ProblemsPage />
 }
--- src/app/companies/(company-wise)/[company-slug]/page.tsx
+++ src/app/companies/(company-wise)/[company-slug]/page.tsx
@@ -1,24 +1,12 @@
-import { type SearchParams, type CompanyParams } from "~/types/company";
+import { type CompanyParams } from "~/types/company";
 import { CompanyPage } from "~/components/company/company-page";
 
 export default async function CompanyWiseQuestion({
-    params,
-    searchParams
+    params
 }: {
     params: Promise<CompanyParams>;
-    searchParams: Promise<SearchParams>;
 }) {
     const { 'company-slug': slug } = await params;
-    const p = await searchParams;
-    const { tags = [] } = p;
-    const { sort = 'frequency', order = 'all', search = '' } = p;
-    return (
-        <CompanyPage
-            sort={sort}
-            order={order}
-            search={search}
-            slug={slug}
-            tags={tags}
-        />
-    );
+
+    return <CompanyPage slug={slug} />;
 }
