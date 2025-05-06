69fad97183dd6f787b03411e7800ce6a8287fdc2
zackozack <id.ayushkryadav@gmail.com>
2025-05-06 07:36:46 +0000

    fix: banner height

--- src/components/ads/banner.tsx
+++ src/components/ads/banner.tsx
@@ -4,13 +4,11 @@ export const AdBanner = () => {
 
     return (
         <div
-            className="justify-center items-center text-white text-center w-full overflow-hidden"
+            className=" flex justify-center items-center text-white text-center w-full overflow-hidden"
             
         >
             <iframe
                 src={`//www.topcreativeformat.com/watchnew?key=93c46aa9fc2321a90646168921d5ff10`}
-                width={728}
-                height={91}
             />
         </div>
     );
