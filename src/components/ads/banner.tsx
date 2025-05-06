5ae3d4ef86e2b6dea08c2b5fd0784d2651d06fce
zackozack <id.ayushkryadav@gmail.com>
2025-05-06 07:39:23 +0000

    fix: banner height

--- src/components/ads/banner.tsx
+++ src/components/ads/banner.tsx
@@ -9,6 +9,7 @@ export const AdBanner = () => {
         >
             <iframe
                 src={`//www.topcreativeformat.com/watchnew?key=93c46aa9fc2321a90646168921d5ff10`}
+                className="w-full h-auto"
             />
         </div>
     );
