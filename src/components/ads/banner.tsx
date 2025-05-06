da7b3881c935181bdb83510301d6ae8606cd9466
zackozack <id.ayushkryadav@gmail.com>
2025-05-06 06:06:21 +0000

    fix: remove unused imports

--- src/components/ads/banner.tsx
+++ src/components/ads/banner.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const AdBanner = () => {
     const banner = useRef<HTMLDivElement>(null);
