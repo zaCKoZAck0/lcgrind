2cd0cbf6dbe4fe904916b0122014598cae2c06a0
zackozack <id.ayushkryadav@gmail.com>
2025-05-06 07:16:51 +0000

    fix: banner overflow

--- src/components/ads/banner.tsx
+++ src/components/ads/banner.tsx
@@ -28,7 +28,7 @@ export const AdBanner = () => {
 
     return (
         <div
-            className="mx-2 my-5 border border-gray-200 justify-center items-center text-white text-center"
+            className="mx-2 my-5 border border-gray-200 justify-center items-center text-white text-center w-full overflow-hidden"
             ref={banner}
         />
     );
